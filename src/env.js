d025f02cb56aa6baee3719eab291f64802db9af6
Nirmal Kumar <nirmal1064@gmail.com>
2024-01-27 18:36:49 +0530

    Support for netlify url in NEXTAUTH

--- src/env.js
+++ src/env.js
@@ -22,9 +22,9 @@ export const env = createEnv({
         ? z.string()
         : z.string().optional(),
     NEXTAUTH_URL: z.preprocess(
-      // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
-      // Since NextAuth.js automatically uses the VERCEL_URL if present.
-      (str) => process.env.VERCEL_URL ?? str,
+      // This makes Vercel/Netlify deployments not fail if you don't set NEXTAUTH_URL
+      // Since NextAuth.js automatically uses the VERCEL_URL/URL if present.
+      (str) => process.env.VERCEL_URL ?? process.env.URL ?? str,
       // VERCEL_URL doesn't include `https` so it cant be validated as a URL
       process.env.VERCEL ? z.string() : z.string().url(),
     ),
